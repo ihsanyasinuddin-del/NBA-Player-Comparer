01ea5eb5d2965bae14e765b3c5af62149c961d60
Ihsan Uddin <ihsanuddin@Mac.lan>
2026-09-04 17:53:47 +0100

    Added error checking for season and player

--- website.py
+++ website.py
@@ -60,15 +60,8 @@ with left:
     player_one = st.text_input("Enter player one ")
 
 
-player_one_id, player_one_name = get_player(player_one)
-player_one_stats = get_stats(player_id=player_one_id, season=selected_season)
-
 with right:
     player_two = st.text_input("Enter player two ")
-
-
-player_two_id, player_two_name = get_player(player_two)
-player_two_stats = get_stats(player_id=player_two_id, season=selected_season)
 
 
 compare = st.button("COMPARE", icon="⚖️", width="stretch")
@@ -113,9 +106,27 @@ def display_stats_and_compare(playeronestats, playertwostats, name1, name2,):
                 with col2:
                     st.metric(f"{stat}", f"{playertwostats[stat]:.1f}")   
 
-if player_two_stats is not None:
-    if compare: 
-        col1, col2 = st.columns(2, border=True)
-        display_stats_and_compare(player_one_stats, player_two_stats, player_one_name, player_two_name)
+if compare: 
+    col1, col2 = st.columns(2, border=True)
+
+    first_package = get_player(player_one)
+    if first_package is None:
+        st.error(f"No results for {player_one} found", icon="❌", )
+    else:
+        player_one_id, player_one_name = first_package
+        player_one_stats = get_stats(player_id=player_one_id, season=selected_season)
+        if player_one_stats is None:
+            st.error(f"{player_one} did not play in that season!", icon="⏳")
+        else:
+            second_package = get_player(player_two)
+            if second_package is None:
+                st.error(f"No results for {player_two} found", icon="❌")
+            else:
+                player_two_id, player_two_name = second_package
+                player_two_stats = get_stats(player_id=player_two_id, season=selected_season)
+                if player_two_stats is None:
+                    st.error(f"{player_two} did not play in that season!", icon="⏳")
+                else:
+                    display_stats_and_compare(player_one_stats, player_two_stats, player_one_name, player_two_name)
 
 

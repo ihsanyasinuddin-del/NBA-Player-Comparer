01643231175076922aa6a853eacff38ec501fa28
Ihsan Uddin <ihsanuddin@Mac.lan>
2026-09-11 12:10:05 +0100

    Added radar chart for both players

--- messing.py
+++ messing.py
@@ -1,10 +1,18 @@
 from nba_api.stats.endpoints import leaguedashplayerstats
-# get the mean ppg of every nba player in the league in that season 
-df = leaguedashplayerstats.LeagueDashPlayerStats(
-    season="2024-25",
+
+def find_percentile(id, desired_stat):
+    df = leaguedashplayerstats.LeagueDashPlayerStats(
+    season="2015-16",
     per_mode_detailed="PerGame"
-).get_data_frames()[0]
-
-dfpoints = df["PTS"]
-
-print(dfpoints.mean())
+    ).get_data_frames()[0]
+    qualifier = df["GP"] * df["MIN"]
+    qualified_df = df[qualifier > 1500]
+    length = len(qualified_df)
+    n = 0 
+    points1 = qualified_df.query(f"PLAYER_ID == {id}")[f"{desired_stat}"].iloc[0]
+    for x in qualified_df[f"{desired_stat}"]:
+        if x < points1:
+            n += 1
+    percentile = (n / length) * 100
+    return percentile
+print(find_percentile(201939, "REB"))
--- pages/more_stats.py
+++ pages/more_stats.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import leaguedashplayerstats
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 st.set_page_config(layout="wide")
 
@@ -19,12 +23,31 @@ def make_line_graph(id1, id2, desired_stat, title):
     st.markdown(f"### {title}" , text_alignment="center" )
     st.line_chart(data=combined_points, x="SEASON_ID", y=[f"{name_one}", f"{name_two}"], x_label="Season", y_label=f"{title}", color=["red", "green"])
 
+season = st.session_state["the_season"]
+df = leaguedashplayerstats.LeagueDashPlayerStats(
+    season=season,
+    per_mode_detailed="PerGame"
+    ).get_data_frames()[0]
+
+def find_percentile(id, desired_stat):
+    qualifier = df["GP"] * df["MIN"]
+    qualified_df = df[qualifier > 1500]
+    length = len(qualified_df)
+    n = 0 
+    points1 = qualified_df.query(f"PLAYER_ID == {id}")[f"{desired_stat}"].iloc[0]
+    for x in qualified_df[f"{desired_stat}"]:
+        if x < points1:
+            n += 1
+    percentile = (n / length) * 100
+    return percentile
+
 if st.session_state.get("player_one_id"):
     player_one_id = st.session_state.get("player_one_id")
     if st.session_state.get("player_two_id"):
         name_one = st.session_state.get("chosen_name")
         name_two = st.session_state.get("chosen_name2")
         player_two_id = st.session_state.get("player_two_id")
+        season = st.session_state["the_season"]
         left, right = st.columns([0.5, 0.5])
         with left:
             make_line_graph(player_one_id, player_two_id, desired_stat="PTS", title="Points Per Game")
@@ -32,6 +55,66 @@ if st.session_state.get("player_one_id"):
             make_line_graph(player_one_id, player_two_id, desired_stat="FG_PCT", title="Field Goal Percentage")
             make_line_graph(player_one_id, player_two_id, desired_stat="FG3_PCT", title="Three-point percentage")
             make_line_graph(player_one_id, player_two_id, desired_stat="REB", title="Rebounds")
+        with right:
+            points_percentile_one = find_percentile(player_one_id, "PTS")
+            ast_percentile_one = find_percentile(player_one_id, "AST")
+            three_point_percentile_one = find_percentile(player_one_id, "FG3_PCT")
+            rebounds_percentile_one = find_percentile(player_one_id, "REB")
+            steal_percentile_one = find_percentile(player_one_id, "STL")
+            blocks_percentile_one = find_percentile(player_one_id, "BLK")
+
+            points_percentile_two = find_percentile(player_two_id, "PTS")
+            ast_percentile_two = find_percentile(player_two_id, "AST")
+            three_point_percentile_two = find_percentile(player_two_id, "FG3_PCT")
+            rebounds_percentile_two = find_percentile(player_two_id, "REB")
+            steal_percentile_two = find_percentile(player_two_id, "STL")
+            blocks_percentile_two = find_percentile(player_two_id, "BLK")
+
+
+            categories = ["points", "assists", "3pt%", "rebounds", "steals", "blocks"]
+            fig = go.Figure()
+            fig.add_trace(go.Scatterpolar(
+                r=[
+                    points_percentile_one,
+                    ast_percentile_one,
+                    three_point_percentile_one,
+                    rebounds_percentile_one,
+                    steal_percentile_one,
+                    blocks_percentile_one
+
+
+
+                ],
+                fill="toself",
+                name=f"{name_one}"
+
+                ))  
+            fig.add_trace(go.Scatterpolar(
+                    r=[
+                        points_percentile_two,
+                        ast_percentile_two,
+                        three_point_percentile_two,
+                        rebounds_percentile_two,
+                        steal_percentile_two,
+                        blocks_percentile_two
+
+                    ],
+                    fill="toself",
+                    name=f"{name_two}"      
+
+
+                        ))
+            fig.update_layout(
+                polar=dict(
+                    radialaxis=dict(
+                        visible=True,
+                        range=[0,100],
+                        tickvals=[20,40,60,80,100]
+                    )
+                )
+            )
+            st.plotly_chart(fig, use_container_width=True)
+
     else:
         st.write("Input two players on the homepage to see more stats!")
 else:
